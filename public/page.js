25649df026db19a332ded0cfcd7d1ec38d6df995
Anna Filin <68apple@gmail.com>
2024-09-12 16:12:35 +0300

    fixing path....////

--- public/page.js
+++ public/page.js
@@ -1,22 +1,18 @@
 import Image from "next/image";
-import image1 from "@/public/about-1.jpg";
-import image2 from "@/public/about-2.jpg";
-import { getCabins } from "../_lib/data-service";
-
-export const revalidate = 86400;
+import portret1 from "@/public/portret1.jpg";
+import portret2 from "@/public/portret2.jpg";
+import portret3 from "@/public/portret3.jpg";
 
 export const metadata = {
   title: "About",
 };
 
-export default async function Page() {
-  const cabins = await getCabins();
-
+export default function Page() {
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
         <h1 className="text-4xl mb-10 text-accent-400 font-medium">
-          Welcome to The Wild Oasis
+          About Filin Art
         </h1>
 
         <div className="space-y-8">
@@ -28,10 +24,10 @@ export default async function Page() {
             and enjoying simple pleasures with family.
           </p>
           <p>
-            Our {cabins.length} luxury cabins provide a cozy base, but the real
-            freedom and peace you&apos;ll find in the surrounding mountains.
-            Wander through lush forests, breathe in the fresh air, and watch the
-            stars twinkle above from the warmth of a campfire or your hot tub.
+            Our 8 luxury cabins provide a cozy base, but the real freedom and
+            peace you&apos;ll find in the surrounding mountains. Wander through
+            lush forests, breathe in the fresh air, and watch the stars twinkle
+            above from the warmth of a campfire or your hot tub.
           </p>
           <p>
             This is where memorable moments are made, surrounded by
@@ -43,27 +39,33 @@ export default async function Page() {
 
       <div className="col-span-2">
         <Image
-          src={image1}
+          src={portret1}
           alt="Family sitting around a fire pit in front of cabin"
           placeholder="blur"
           quality={80}
         />
       </div>
 
-      <div className="relative aspect-square col-span-2">
-        <Image
-          src="/about-2.jpg"
-          fill
-          className="object-cover"
-          alt="Family that manages The Wild Oasis"
-        />
-      </div>
-
       <div className="col-span-3">
         <h1 className="text-4xl mb-10 text-accent-400 font-medium">
-          Managed by our family since 1962
+          P(A)rt of My Inner Owl
         </h1>
-
+        <div className="space-y-8">
+          <p>
+            Since 1962, The Wild Oasis has been a cherished family-run retreat.
+            Started by our grandparents, this haven has been nurtured with love
+            and care, passing down through our family as a testament to our
+            dedication to creating a warm, welcoming environment.
+          </p>
+          <div className="relative aspect-square col-span-2">
+            <Image
+              src={portret2}
+              fill
+              className="object-cover"
+              alt="Family that manages The Wild Oasis"
+            />
+          </div>
+        </div>
         <div className="space-y-8">
           <p>
             Since 1962, The Wild Oasis has been a cherished family-run retreat.
@@ -79,13 +81,21 @@ export default async function Page() {
             Wild Oasis soon, where tradition meets tranquility, and every visit
             is like coming home.
           </p>
+          <div className="relative aspect-square col-span-2">
+            <Image
+              src={portret3}
+              fill
+              className="object-cover"
+              alt="Family that manages The Wild Oasis"
+            />
+          </div>
 
           <div>
             <a
-              href="/cabins"
-              className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+              href="/gallery"
+              className="inline-block mt-4 px-8 py-5 text-primary-800 text-lg font-semibold "
             >
-              Explore our luxury cabins
+              Explore my artworks.
             </a>
           </div>
         </div>
